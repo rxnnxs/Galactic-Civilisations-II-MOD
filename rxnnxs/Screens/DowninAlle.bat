d0bc57e59931c9b60bc99194dcb12924f1c0c771
rxnnxs <fred@egalx.com>
2024-01-24 20:36:47 +0100

    bilder, gimp und batch dateien hinzu
    sowie tolle knöpfe schon eingebaut

---
+++ rxnnxs/Screens/DowninAlle.bat
@@ -0,0 +1,120 @@
+7z a -tzip AlienMsgWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip AlienMsgWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip AlienMsgWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip CampaignSaveListWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip CampaignSaveListWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip CampaignSaveListWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip CampaignWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip CampaignWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip CampaignWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip ColorPickerWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip ColorPickerWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip ColorPickerWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip CustomRaceWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip CustomRaceWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip CustomRaceWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip CustomShipStyleWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip CustomShipStyleWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip CustomShipStyleWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip DomesticGovntWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip DomesticGovntWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip DomesticGovntWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip EventWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip EventWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip EventWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip FleetManagerWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip FleetManagerWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip FleetManagerWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip GalaxyWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip GalaxyWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip GalaxyWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip MessageBoxWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip MessageBoxWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip MessageBoxWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip MetaverseWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip MetaverseWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip MetaverseWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip MiningBaseWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip MiningBaseWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip MiningBaseWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip MissionWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip MissionWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip MissionWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip OpponentWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip OpponentWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip OpponentWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip OptionsWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip OptionsWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip OptionsWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip PlaceAgentWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip PlaceAgentWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip PlaceAgentWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip PurchaseWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip PurchaseWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip PurchaseWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip RaceWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip RaceWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip RaceWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip SaveGameWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip SaveGameWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip SaveGameWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip SaveItemWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip SaveItemWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip SaveItemWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip ScenarioWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip ScenarioWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip ScenarioWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip ShipDesignFocusWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip ShipDesignFocusWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip ShipDesignFocusWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip ShipDetailsWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip ShipDetailsWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip ShipDetailsWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip StarbaseDetailsWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip StarbaseDetailsWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip StarbaseDetailsWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip StarbaseWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip StarbaseWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip StarbaseWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip TournamentWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip TournamentWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip TournamentWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip TroopLoadWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip TroopLoadWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip TroopLoadWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip TutorialWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip TutorialWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip TutorialWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+
+7z a -tzip DowngradeWnd.dxpack Metal_Button_Medium_Bottom_Left_Down.png
+7z a -tzip DowngradeWnd.dxpack Metal_Button_Medium_Bottom_Center_Down.png
+7z a -tzip DowngradeWnd.dxpack Metal_Button_Medium_Bottom_Right_Down.png
+pause
