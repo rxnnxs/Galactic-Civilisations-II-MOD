726ca63e853ecd72deaa5f1a2628f3b246dbb814
rxnnxs <fred@egalx.com>
2024-01-25 15:12:25 +0100

    Neue Tasten
    Neue Gimp Sachen, Knöpfe usw.

---
+++ rxnnxs/Screens/MetalRound_ALLE.bat
@@ -0,0 +1,16 @@
+7z a -tzip GNNWnd.dxpack Metal_Round_Button_Over.png
+7z a -tzip GNNWnd.dxpack Metal_Round_Button_Down.png
+7z a -tzip GNNWnd.dxpack Metal_Round_Button_Up.png
+
+7z a -tzip SummaryWnd.dxpack Metal_Round_Button_Over.png
+7z a -tzip SummaryWnd.dxpack Metal_Round_Button_Down.png
+7z a -tzip SummaryWnd.dxpack Metal_Round_Button_Up.png
+
+7z a -tzip ShipReportWnd.dxpack Metal_Round_Button_Over.png
+7z a -tzip ShipReportWnd.dxpack Metal_Round_Button_Down.png
+7z a -tzip ShipReportWnd.dxpack Metal_Round_Button_Up.png
+
+7z a -tzip FleetCombatWnd.dxpack Metal_Round_Button_Over.png
+7z a -tzip FleetCombatWnd.dxpack Metal_Round_Button_Down.png
+7z a -tzip FleetCombatWnd.dxpack Metal_Round_Button_Up.png
+pause
---
+++ rxnnxs/Screens/Single_ALLE.bat
@@ -0,0 +1,25 @@
+7z a -tzip QuarterlyWnd.dxpack Metal_Medium_Single_Button_Down.png
+7z a -tzip QuarterlyWnd.dxpack Metal_Medium_Single_Button_Over.png
+7z a -tzip QuarterlyWnd.dxpack Metal_Medium_Single_Button_Up.png
+
+7z a -tzip InvasionWnd.dxpack Metal_Medium_Single_Button_Down.png
+7z a -tzip InvasionWnd.dxpack Metal_Medium_Single_Button_Over.png
+7z a -tzip InvasionWnd.dxpack Metal_Medium_Single_Button_Up.png
+
+7z a -tzip HighScoresWnd.dxpack Metal_Medium_Single_Button_Down.png
+7z a -tzip HighScoresWnd.dxpack Metal_Medium_Single_Button_Over.png
+7z a -tzip HighScoresWnd.dxpack Metal_Medium_Single_Button_Up.png
+
+7z a -tzip ForeignWnd.dxpack Metal_Medium_Single_Button_Down.png
+7z a -tzip ForeignWnd.dxpack Metal_Medium_Single_Button_Over.png
+7z a -tzip ForeignWnd.dxpack Metal_Medium_Single_Button_Up.png
+
+7z a -tzip EthicsWnd.dxpack Metal_Medium_Single_Button_Down.png
+7z a -tzip EthicsWnd.dxpack Metal_Medium_Single_Button_Over.png
+7z a -tzip EthicsWnd.dxpack Metal_Medium_Single_Button_Up.png
+
+7z a -tzip AlertWnd.dxpack Metal_Medium_Single_Button_Down.png
+7z a -tzip AlertWnd.dxpack Metal_Medium_Single_Button_Over.png
+7z a -tzip AlertWnd.dxpack Metal_Medium_Single_Button_Up.png
+
+pause
